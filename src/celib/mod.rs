b8b45f6210080b065c4b85e9a69cfaffb2a4626e
Oğuz Kurt <kurt.oguz@outlook.com>
2022-05-11 14:49:43 +0300

    randomInt test

--- src/celib/mod.rs
+++ src/celib/mod.rs
@@ -4,5 +4,6 @@ extern "C" {
     pub fn os_HomeUp();
     pub fn os_DrawStatusBar();
     pub fn os_PutStrFull(str: *const [u8]);
-    pub fn os_GetCSC() -> i8;
+    pub fn os_GetCSC() -> u8;
+    pub fn randInt(min: usize, max: usize) -> usize;
 }
--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,30 @@
 #![no_main]
 
 mod celib;
+mod format;
 
 use celib::*;
+use format::write_to;
+
+use core::fmt::Write;
 
 #[no_mangle]
 unsafe fn main() {
     os_ClrLCD();
     os_HomeUp();
     os_DrawStatusBar();
-    os_PutStrFull(b"Hello from Rust!\x00");
+    let mut buf = [0u8; 32];
+    let mut w = write_to::WriteTo::new(&mut buf);
+    let r = randInt(3, 5);
+    let s1 = b"Hello from Rust!\x00";
+    os_PutStrFull(s1);
+    w.write_fmt(format_args!("{}", r)); 
+    let str_bytes = w.as_str().unwrap().as_bytes();
+    let mut buf2 = [0u8; 32];
+    for i in 0..str_bytes.len() {
+        buf2[i] ^= str_bytes[i];
+    }
+    os_PutStrFull(&buf2);
     while os_GetCSC() == 0 {}
 }
 
